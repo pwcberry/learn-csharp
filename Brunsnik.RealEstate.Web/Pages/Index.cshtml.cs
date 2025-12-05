7b9ae3a4e8178bc246777d0e68fd1deac122de23
Peter Berry <pwcberry@gmail.com>
2025-12-05 21:38:28 +1100

    Set up ASP.NET site

---
+++ Brunsnik.RealEstate.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Brunsnik.RealEstate.Web.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
